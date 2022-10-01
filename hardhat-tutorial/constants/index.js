4ad03a4301cdbc10d7feb02a6824a031401068b3
FenixDrago <202170@astanait.edu.kz>
2022-10-01 14:42:33 +0600

    good

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2de3f49dab442e1c5f69ea2e9363f50f96310749";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "my-app-psi-six.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
